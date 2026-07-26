2d34a7528f406dc601f7a94a7584d4cc234c25e9
443eb9#C <443eb9@gmail.com>
2026-07-26 12:23:01 +0800

    resolve forgotten conflict

--- crates/gpui_platform/src/gpui_platform.rs
+++ crates/gpui_platform/src/gpui_platform.rs
@@ -34,24 +34,12 @@ pub fn web_init() {
 
 /// Returns the default [`Platform`] for the current OS.
 pub fn current_platform(headless: bool) -> Rc<dyn Platform> {
-<<<<<<< HEAD
-    #[cfg(all(
-        any(
-            target_os = "windows",
-            target_os = "macos",
-            target_os = "linux",
-            target_os = "freebsd"
-        ),
-        feature = "winit"
-    ))]
-=======
     #[cfg(all(target_os = "macos", not(feature = "winit")))]
     {
         Rc::new(gpui_macos::MacPlatform::new(headless))
     }
 
     #[cfg(all(any(target_os = "windows", target_os = "macos"), feature = "winit"))]
->>>>>>> winit-backend
     {
         Rc::new(gpui_winit::WinitUnifiedPlatform::new(headless))
     }
